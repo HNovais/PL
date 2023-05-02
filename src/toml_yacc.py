8811519eeb3b7e4bdc5432a5c9c2af10de4179ac
Diogo Costa <73493561+dioguinsequi@users.noreply.github.com>
2023-05-02 12:34:21 +0100

    Update toml_yacc.py

--- src/toml_yacc.py
+++ src/toml_yacc.py
@@ -1,48 +1,79 @@
 import ply.yacc as yacc
-from toml_lex import tokens
+from lexer import tokens
+
+def p_file(p):
+    '''file : toms'''
+    p[0] = p[1]
+    
+
+def p_toms(p):
+    '''toms : toms tom
+            | tom'''
+    if(len(p)==2):
+        p[0] = p[1]
+    else:
+        p[1].append(p[2])
+        p[0] = p[1]
 
 
 def p_tom(p):
     '''tom : keyvalue
             | tabela
-            | title
             | comment'''
+            # | title'''
     p[0] = p[1]
-    print(p[0])
+    
 
 def p_comment(p):
     '''comment : COMMENT'''
     p[0] = p[1]
-def p_title(p):
-    '''title : TITLE_VALUE'''
+
+def p_keyvalue(p):
+    '''keyvalue : key EQUAL value'''
+    p[0] = (p[1],p[3])
+
+
+def p_key(p):
+    '''key : quotedkey
+            | dottedkey
+            | barekey'''
+            #| title'''
+
+#def p_title(p):
+ #   '''title : TITLE'''
+  #  p[0] = p[1]
+
+def p_barekey(p):
+    '''barekey : TEXT'''
     p[0] = p[1]
 
-def p_tabela(p):
-    '''tabela : TABLE
-                | tabela'''
+def p_dottedkey(p):
+    '''dottedkey : TEXT DOT TEXT'''
+    p[0] = (p[1],p[3])
+
+def p_quotedkey(p):
+    '''quotedkey : QUOTED_STRING'''
     p[0] = p[1][1:-1]
 
 
+def p_value(p):
+    ''' value : QUOTED_STRING
+              | PLICA_STRING
+              | ML_QUOTED_STRING
+              | ML_PLICA_STRING
+              | INT
+              | FLOAT
+              | BOOLEAN
+              | O_DATE_TIME
+              | L_DATE_TIME
+              | LOCAL_DATE
+              | LOCAL_TIME
+              | array'''
+    p[0] = p[1]
 
-def p_keyvalue(p):
-    '''keyvalue : BARE_KEY value
-                | DOTTED_KEY value
-                | QUOTED_KEY value'''
-
-    p[0] = (p[1], p[2])
-
-
-# def p_barekey(p):
-#   '''barekey : BARE_KEY value'''
-#  p[0] = (p[1], p[2])
-# print("bare"+ str(p[0]))
-
-
-# def p_dottedkey(p):
-#   '''dottedkey : ID DOT ID value'''
-#  p[0] = (p[1], p[3], p[4])
-# print("ola")
-# print("dotted"+ str(p[0]))
+def p_array(p):
+    '''array : L_SQUARE_BRACKET elementos R_SQUARE_BRACKET'''
+    p[0] = p[2]
 
 def p_elementos(p):
     '''elementos : value
@@ -53,39 +84,41 @@ def p_elementos(p):
         p[3].insert(0, p[1])
         p[0] = p[3]
 
-def p_array(p):
-    '''array : ARRAY_START elementos ARRAY_END'''
-    p[0] = p[2]
-
-def p_value(p):
-    '''value : string
-            | BOOLEAN
-            | array
-            | number
-            | data'''
+def p_tables(p):
+    '''tables : tabela
+                | inlinetabela'''
+                #| arraytabela'''
     p[0] = p[1]
 
-def p_data(p):
-    '''data : ODT
-            | LDT
-            | LOCAL_DATE
-            | LOCAL_TIME'''
+
+def p_inlinetabela(p):
+    '''inlinetabela : L_BRACKET inlinecont R_BRACKET'''
     p[0] = p[1]
 
-def p_number(p):
-    '''number : INT
-                | FLOAT
-                | HEX
-                | OCT
-                | BIN'''
+def p_inlinecont(p):
+    '''inlinecont : keyvalue
+                    | keyvalue COMMA inlinecont'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[3].insert(0, p[1])
+        p[0] = p[3]
+
+
+def p_tabela(p):
+    '''tabela : L_SQUARE_BRACKET subtabela R_SQUARE_BRACKET'''
     p[0] = p[1]
 
-def p_string(p):
-    '''string : BASIC_STRING
-                | MLBASIC_STRING
-                | MLLITERAL_STRING
-                | LITERAL_STRING'''
-    p[0] = p[1]
+def p_subtabela(p):
+    '''subtabela : TEXT
+                | TEXT DOT subtabela'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[3].insert(0, p[1])
+        p[0] = p[3]
+
+
 
 def p_error(p):
     if p:
