7dc0ceda22762ff069f6c2e0efd427ddb7384fec
Hugo Novais <a96267@alunos.uminho.pt>
2023-05-16 11:44:02 +0100

    sdf

--- src/yacc.py
+++ src/yacc.py
@@ -2,28 +2,47 @@ import json
 import ply.yacc as yacc
 from lexer import tokens
 
-is_nested_key = False
+tables = []
+currentTable = None
 
 def p_start(p):
     '''start : pairs
-             | table'''
+             | tables'''
     p[0] = p[1]
+
+def p_tables(p):
+    ''' tables : table
+               | tables table'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[0] = {}
+        p[0].update(p[1])
+        p[0].update(p[2])
 
 def p_table(p):
     '''table : L_SQUARE_BRACKET key R_SQUARE_BRACKET NEWLINE pairs
              | NEWLINE L_SQUARE_BRACKET key R_SQUARE_BRACKET NEWLINE pairs'''
-    if len(p) == 6:
-        p[0] = {p[2]: p[5]}
-    else:
-        p[0] = {p[3]: p[6]}
 
-def p_pairs(p):
-    ''' pairs : pair 
-              | pairs pair '''
-    if len(p) == 2:
-        p[0] = [p[1]]
+    if len(p) == 6:
+        key = p[2]
+        pairs = p[5]
     else:
-        p[0] = p[1] + [p[2]]
+        key = p[3]
+        pairs = p[6]
+    
+    table_dict = {key: pairs}
+    tables.append(table_dict)
+    p[0] = table_dict
+    
+def p_pairs(p):
+    ''' pairs : pair
+              | pairs pair'''
+    if len(p) == 2:
+        p[0] = p[1]
+    else:
+        p[1].update(p[2])
+        p[0] = p[1]
 
 def p_pair(p):
     '''pair : key EQUAL value 
@@ -32,23 +51,20 @@ def p_pair(p):
 
 def p_key(p):
     ''' key : bare_key
-            | quoted_key
             | dotted_key'''
     p[0] = p[1]
 
 def p_bare_key(p):
     ''' bare_key : TEXT
-                 | INT'''
-    p[0] = p[1]
-
-def p_quoted_key(p):
-    ''' quoted_key : QUOTED_STRING
-                   | PLICA_STRING'''
+                 | INT
+                 | QUOTED_STRING
+                 | PLICA_STRING'''
     p[0] = p[1]
 
 def p_dotted_key(p):
-    'dotted_key : key DOT key'
-    print(p[1], p[3])
+    ''' dotted_key : bare_key DOT key '''
+    p[1] = {p[3] : None}
+    p[0] = p[1]
 
 def p_value(p):
     ''' value : QUOTED_STRING
@@ -90,6 +106,8 @@ with open("file.toml", encoding="utf-8") as f:
     content = f.read()
 
 result = parser.parse(content)
+
+print(tables)
 
 with open("output.json", "w") as f:
     f.write(json.dumps(result, indent=2)) 
